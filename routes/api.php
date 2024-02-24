a361ab1150b47c07824142a8dcbd4c308b8e6e4e
Yousef Nahhas <75533977+MRyousef1998@users.noreply.github.com>
2024-02-24 04:26:44 -0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,17 +20,4 @@ use App\Mail\SinupEmail;
 
 use Illuminate\Support\Facades\Validator;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-Route::post('auth/register', [AuthController::class,'register']);
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//
