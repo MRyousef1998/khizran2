16f6aecf04cba247721d297cf3212577d36120fd
yousefnahhas06@gmail.com <yousefnahhas06@gmail.com>
2024-08-20 00:46:10 +0400

    aster

--- app/Http/Controllers/Api/ExportOrder.php
+++ app/Http/Controllers/Api/ExportOrder.php
@@ -129,7 +129,7 @@ class ExportOrder extends Controller
            // $detail=OrderDetail::where("orders_id",$order_id);
             $machines =DB::table('products')->
            leftJoin('product_details', 'product_details.id', '=', 'products.product_details_id')->leftJoin('product_groups', 'product_details.group_id', '=', 'product_groups.id')->leftJoin('product_companies', 'product_details.company_id', '=', 'product_companies.id')
-           ->leftJoin('statuses', 'products.statuses_id', '=', 'statuses.id') ->Join('order_product', 'products.id', '=', 'order_product.products_id')->where("order_product.orders_id", $request->order_id)->where("product_details.category_id", 1)
+           ->leftJoin('statuses', 'products.statuses_id', '=', 'statuses.id') ->Join('order_product', 'products.id', '=', 'order_product.products_id')->where("order_product.orders_id", $request->order_id)->where("product_details.category_id", $request->category_id)
            ->selectRaw('product_details.id,company_name,product_name,group_name,country_of_manufacture,count(products.product_details_id) as aggregate,count(products.box_id) as box_count,product_details.image_name')
            ->groupBy('product_details.id','company_name','product_name','country_of_manufacture','group_name','product_details.image_name')->get();
            $myMachines = $this->paginate([$machines]);
